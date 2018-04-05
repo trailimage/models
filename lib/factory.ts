6bc1dc3eebb5af9ca62a635cf99c5d1f1759b8a5
Jason Abbott <dev@trailimage.com>
2018-04-04 21:29:25 -0600

    factory assign methods

--- lib/factory.ts
+++ lib/factory.ts
@@ -1,14 +1,29 @@
 import { PhotoBlog } from './photo-blog';
-import { EXIF, Photo, Post } from '../';
+import { EXIF, Photo, Post, photoBlog } from '../';
 
 /**
  * Main load method and lazy-load methos for particular data.
  */
 export interface ModelFactory {
-   load(): PhotoBlog;
+   load(emptyIfLoaded?: boolean): PhotoBlog;
    getEXIF(photoID: string): Promise<EXIF>;
    getPostWithPhoto(this: PhotoBlog, photo: Photo | string): Promise<Post>;
    getPhotosWithTags(tags: string | string[]): Promise<Photo[]>;
    getPostInfo(this: Post): Promise<Post>;
    getPostPhotos(this: Post): Promise<Post>;
+}
+
+export function assignPostFactoryMethods(
+   factory: ModelFactory,
+   post: Post
+): Post {
+   post.getInfo = factory.getPostInfo.bind(post);
+   post.getPhotos = factory.getPostPhotos.bind(post);
+   return post;
+}
+
+export function assignFactoryMethods(factory: ModelFactory): void {
+   photoBlog.getEXIF = factory.getEXIF.bind(photoBlog);
+   photoBlog.getPostWithPhoto = factory.getPostWithPhoto.bind(photoBlog);
+   photoBlog.getPhotosWithTags = factory.getPhotosWithTags.bind(photoBlog);
 }
