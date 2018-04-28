986e02ebf3e371cc007250129abbba3a80765f44
Jason Abbott <dev@trailimage.com>
2018-04-27 19:35:20 -0600

    export owner

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ export { PhotoSize } from './lib/photo-size';
 export { blog, PhotoBlog } from './lib/photo-blog';
 export { VideoInfo } from './lib/video-info';
 export { EXIF } from './lib/exif';
+export { owner } from './lib/json-ld';
 export { PostProvider, MapProvider, VideoProvider } from './lib/providers';
 export {
    Configuration as ModelConfig,
