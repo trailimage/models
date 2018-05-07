98c292dff3e7b04b0e7b1bba27fb33a6a39e0d34
Jason Abbott <dev@trailimage.com>
2018-05-06 21:25:30 -0600

    tests

--- lib/photo-blog.test.ts
+++ lib/photo-blog.test.ts
@@ -1,5 +1,8 @@
 import '@toba/test';
 import { PhotoBlog } from '../';
+import { mockPosts } from './.test-data';
+
+const [post1, post2, post3, post4] = mockPosts;
 
 test('Ensures only one instance exists', () => {
    let e: Error;
@@ -12,4 +15,41 @@ test('Ensures only one instance exists', () => {
    expect(b).toBeUndefined();
    expect(e).toBeDefined();
    expect(e.message).toBe('PhotoBlog instance already exists');
+});
+
+test('is linked to next and previous posts', () => {
+   expect(post1.previous).toBeNull();
+   expect(post2.previous).toBe(post1);
+   expect(post1.next).toBe(post2);
+   expect(post2.next).toBeDefined();
+});
+
+// test('is connected to parts of a series', () => {
+//    expect(post1.totalParts).toBe(0);
+//    expect(post2.totalParts).toBe(2);
+//    expect(post1.part).toBe(0);
+//    expect(post2.part).toBe(2);
+//    expect(post1.subTitle).toBeNull();
+//    expect(post2.subTitle).toBe('Lowlands');
+//    expect(post1.previousIsPart).toBe(false);
+//    expect(post2.previousIsPart).toBe(true);
+//    expect(post1.isPartial).toBe(false);
+//    expect(post2.isPartial).toBe(true);
+//    expect(post2.isSeriesStart).toBe(false);
+//    expect(post2.previous.isSeriesStart).toBe(true);
+// });
+
+// test('combines series and post ttestle', () => {
+//    expect(post2.name()).toBe('Owyhee Snow and Sand: Lowlands');
+// });
+
+test('can be removed from a series', () => {
+   post2.ungroup();
+
+   expect(post2.subTitle).toBeNull();
+   expect(post2.isPartial).toBe(false);
+   expect(post2.totalParts).toBe(0);
+   expect(post2.part).toBe(0);
+   expect(post2.title).toBe(post2.originalTitle);
+   expect(post2.previousIsPart).toBe(false);
 });
--- lib/post.test.ts
+++ lib/post.test.ts
@@ -1,6 +1,8 @@
 import '@toba/test';
+import { AtomEntry } from '@toba/feed';
 import { mockPosts } from './.test-data';
-import './provider.test';
+
+const [post1, post2, post3, post4] = mockPosts;
 
 // test.skip('normalizes provider values', () => {
 //    // mock Flickr response values are all the same
@@ -13,53 +15,18 @@ import './provider.test';
 // });
 
 test('can be matched to a key', () => {
-   expect(mockPosts[0].hasKey('blah')).toBe(false);
-   expect(mockPosts[1].hasKey('key1')).toBe(true);
+   expect(post1.hasKey('blah')).toBe(false);
+   expect(post2.hasKey('key1')).toBe(true);
 });
 
 test('identifies category membership', () => {
-   expect(mockPosts[0].hasCategories).toBe(true);
-   expect(mockPosts[3].hasCategories).toBe(false);
+   expect(post1.hasCategories).toBe(true);
+   expect(post4.hasCategories).toBe(false);
 });
-
-// test('is linked to next and previous posts', () => {
-//    expect(post1.previous).toBeDefined();
-//    expect(post2.previous).toBeDefined();
-//    expect(post1.next).toBeDefined();
-//    expect(post2.next).toBeDefined();
-// });
-
-// test('is connected to parts of a series', () => {
-//    expect(post1.totalParts).toBe(0);
-//    expect(post2.totalParts).toBe(2);
-//    expect(post1.part).toBe(0);
-//    expect(post2.part).toBe(2);
-//    expect(post1.subTitle).toBeNull();
-//    expect(post2.subTitle).toBe('Lowlands');
-//    expect(post1.previousIsPart).toBe(false);
-//    expect(post2.previousIsPart).toBe(true);
-//    expect(post1.isPartial).toBe(false);
-//    expect(post2.isPartial).toBe(true);
-//    expect(post2.isSeriesStart).toBe(false);
-//    expect(post2.previous.isSeriesStart).toBe(true);
-// });
-
-// test('combines series and post ttestle', () => {
-//    expect(post2.name()).toBe('Owyhee Snow and Sand: Lowlands');
-// });
-
-// test('can be removed from a series', () => {
-//    post2.ungroup();
-
-//    expect(post2.subTitle).toBeNull();
-//    expect(post2.isPartial).toBe(false);
-//    expect(post2.totalParts).toBe(0);
-//    expect(post2.part).toBe(0);
-//    expect(post2.title).toBe(post2.originalTitle);
-//    expect(post2.previousIsPart).toBe(false);
-// });
 
 test('can be emptied', () => {
-   mockPosts[0].empty();
-   expect(mockPosts[0].updatedOn).toBeNull();
+   post1.empty();
+   expect(post1.updatedOn).toBeNull();
 });
+
+test('writes RSS data', () => {});
